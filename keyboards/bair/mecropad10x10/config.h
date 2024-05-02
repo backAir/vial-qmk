284d45d8174e2a00c30b591a25de4c892a449f6a
Anthony <72553201+backAir@users.noreply.github.com>
2024-05-02 17:57:28 +0200

    mecropad10x10

---
+++ keyboards/bair/mecropad10x10/config.h
@@ -0,0 +1,28 @@
+// Copyright 2023 Anthony (@bair)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+#define VIAL_KEYBOARD_UID {0xA6, 0x95, 0x15, 0x6D, 0x1B, 0x83, 0x97, 0x5B}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
