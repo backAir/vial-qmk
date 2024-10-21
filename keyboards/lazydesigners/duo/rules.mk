df1a69603936455af3b35226962a366cfbdcb63d
Anthony <72553201+backAir@users.noreply.github.com>
2024-10-21 13:13:26 +0200

    update

---
+++ keyboards/lazydesigners/duo/rules.mk
@@ -0,0 +1,19 @@
+# Build Options
+# change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+<<<<<<< HEAD:keyboards/stenokeyboards/the_uni/rp_2040/rules.mk
+AUDIO_ENABLE = no           # Audio output
+STENO_ENABLE = yes
+VIA_ENABLE = yes
+VIAL_ENABLE=yes
+=======
+AUDIO_ENABLE = no           # Audio output
+>>>>>>> upstream/vial:keyboards/lazydesigners/duo/rules.mk
